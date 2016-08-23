999a113182590462a1e99e7772d536b9b09bbb26
Glauber Gasparotto <glauberj@cit010051.cit>
2016-08-22 22:11:05 -0300

    ajustes

---
+++ SysCommand/Core/Command.cs
@@ -0,0 +1,18 @@
+﻿using Fclp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace SysCommand
+{
+    public abstract class Command
+    {
+        public int OrderExecution { get; set; }
+        public bool OnlyInDebug { get; set; }
+        public bool UsePrefixInAllMethods { get; set; }
+        public string PrefixMethods { get; set; }
+        public bool OnlyMethodsWithAttribute { get; set; }
+        public bool OnlyPropertiesWithAttribute { get; set; }
+    }
+}
